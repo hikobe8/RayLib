74b28c14c7b3eca0be13b146b1692d7bc877c377
hikobe8@163.com <139821.yy>
2018-05-07 19:38:38 +0800

    create LoadingViewManager

--- app/src/main/java/com/ray/raylib/MainActivity.java
+++ app/src/main/java/com/ray/raylib/MainActivity.java
@@ -1,7 +1,9 @@
 package com.ray.raylib;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.ViewGroup;
+import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ViewGroup view = findViewById(android.R.id.content);
+        ProgressBar progressBar = new ProgressBar(this);
+        view.addView(progressBar);
     }
 }
---
+++ lib/src/main/java/com/ray/lib/loading/LoadingViewManager.java
@@ -0,0 +1,12 @@
+package com.ray.lib.loading;
+
+/***
+ *  Author : ryu18356@gmail.com
+ *  Create at 2018-05-07 19:16
+ *  description : loading view logic controller
+ */
+public class LoadingViewManager {
+
+     
+
+}
