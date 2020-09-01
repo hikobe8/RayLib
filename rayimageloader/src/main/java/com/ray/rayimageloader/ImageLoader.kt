fa97c94a175410fe877deeaf7c46eec2006eb551
hikobe8 <hikobe8@163.com>
2020-09-01 14:50:04 +0800

    ImageLoader v0.2 单一职责拆分缓存模块

--- rayimageloader/src/main/java/com/ray/rayimageloader/ImageLoader.kt
+++ rayimageloader/src/main/java/com/ray/rayimageloader/ImageLoader.kt
@@ -15,26 +15,16 @@ import java.util.concurrent.Executors
  */
 class ImageLoader {
 
-    private lateinit var mBitmapCache: LruCache<String, Bitmap>
-
     private val mExecutor = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors())
 
-    init {
-        initImageCache()
-    }
+    private val mImageCache = ImageCache()
 
-    private fun initImageCache() {
-        val totalMemory = Runtime.getRuntime().totalMemory()
-        mBitmapCache = object : LruCache<String, Bitmap>((totalMemory * .4f).toInt()) {
-            override fun sizeOf(key: String?, value: Bitmap?): Int {
-                return value?.run {
-                    (rowBytes * height / 1024f).toInt()
-                } ?: 0
-            }
+    fun displayImage(url: String, imageView: ImageView) {
+        val cache = mImageCache.get(url)
+        if (cache != null) {
+            imageView.setImageBitmap(cache)
+            return
         }
-    }
-
-    fun displayImage(url:String, imageView:ImageView) {
         imageView.tag = url
         mExecutor.submit(Runnable {
             val bitmap = downloadImage(url) ?: return@Runnable
@@ -43,15 +33,42 @@ class ImageLoader {
                     imageView.setImageBitmap(bitmap)
                 }
             }
-            mBitmapCache.put(url, bitmap)
+            mImageCache.put(url, bitmap)
         })
     }
 
-    fun downloadImage(url:String): Bitmap? {
+    fun downloadImage(url: String): Bitmap? {
         val conn = URL(url).openConnection() as HttpURLConnection
         val bitmap = BitmapFactory.decodeStream(conn.inputStream)
         conn.disconnect()
         return bitmap
+    }
+
+}
+
+class ImageCache {
+
+    private lateinit var mImageCache: LruCache<String, Bitmap>
+
+    init {
+        initImageCache()
+    }
+
+    private fun initImageCache() {
+        val totalMemory = Runtime.getRuntime().totalMemory()
+        mImageCache = object : LruCache<String, Bitmap>((totalMemory * .4f).toInt()) {
+            override fun sizeOf(key: String?, value: Bitmap?): Int {
+                return value?.run {
+                    (rowBytes * height / 1024f).toInt()
+                } ?: 0
+            }
+        }
+    }
+
+    fun get(url: String): Bitmap? = mImageCache.get(url)
+
+    fun put(url: String, bitmap: Bitmap) {
+        mImageCache.put(url, bitmap)
     }
 
 }
