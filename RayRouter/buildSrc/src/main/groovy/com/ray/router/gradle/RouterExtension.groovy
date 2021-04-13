3ce0e8e3e1716626db67501c85add512c024d217
hikobe8 <hikobe8@163.com>
2021-04-13 17:12:26 +0800

    路由插件参数化配置

---
+++ RayRouter/buildSrc/src/main/groovy/com/ray/router/gradle/RouterExtension.groovy
@@ -0,0 +1,5 @@
+package com.ray.router.gradle
+
+class RouterExtension {
+    String wikiDir
+}
--- RayRouter/buildSrc/src/main/groovy/com/ray/router/gradle/RouterPlugin.groovy
+++ RayRouter/buildSrc/src/main/groovy/com/ray/router/gradle/RouterPlugin.groovy
@@ -7,6 +7,11 @@ class RouterPlugin implements Plugin<Project> {
 
     @Override
     void apply(Project project) {
-        println "this is from RouterPlugin apply"
+        println "this is from RouterPlugin apply, project = ${project.name}"
+        project.getExtensions().create("router", RouterExtension)
+        project.afterEvaluate {
+            RouterExtension extension = project["router"]
+            println("用户配置的wiki目录为：${extension.wikiDir}")
+        }
     }
 }
