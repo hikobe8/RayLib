57541d2eae1f7db61d0f34ec4c74c75e194bfc1b
hikobe8 <hikobe8@163.com>
2021-05-19 17:03:59 +0800

    创建注解工程

--- RayRouter/app/src/main/java/com/ray/rayrouter/MainActivity.kt
+++ RayRouter/app/src/main/java/com/ray/rayrouter/MainActivity.kt
@@ -2,7 +2,12 @@ package com.ray.rayrouter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.ray.router.annotations.Destination
 
+@Destination(
+    url = "router://home",
+    description = "应用主页"
+)
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
---
+++ RayRouter/router-annotations/src/main/java/com/ray/router/annotations/Destination.java
@@ -0,0 +1,31 @@
+package com.ray.router.annotations;
+
+import com.sun.istack.internal.NotNull;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/*
+ * 当前注解可以标记在类文件上面
+ */
+@Target(ElementType.TYPE)
+/*
+ * 当前注解的生命周期为class文件,编译时期
+ */
+@Retention(RetentionPolicy.CLASS)
+public @interface Destination {
+
+    /**
+     * 当前页面的url
+     * @return 页面的url
+     */
+    String url();
+
+    /**
+     * 页面的描述
+     * @return 页面的描述，比如 "我的收藏"
+     */
+    String description();
+}
