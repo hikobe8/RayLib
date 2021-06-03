e61236a7cea8b4496bdebb92ebc794343e5be6a2
hikobe8 <hikobe8@163.com>
2021-06-03 18:21:22 +0800

    生成路由wiki markdown文档

--- RayRouter/buildSrc/src/main/groovy/com/ray/router/gradle/RouterPlugin.groovy
+++ RayRouter/buildSrc/src/main/groovy/com/ray/router/gradle/RouterPlugin.groovy
@@ -1,5 +1,6 @@
 package com.ray.router.gradle
 
+import groovy.json.JsonSlurper
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 
@@ -14,10 +15,55 @@ class RouterPlugin implements Plugin<Project> {
             }
         }
 
+        //执行clean 任务的时候清除上一次生成的router mapping目录的所有文件
+        project.clean.doFirst {
+            File routerMappingDir = new File(project.rootProject.projectDir, "router_mapping")
+            if (routerMappingDir.exists()) {
+                routerMappingDir.deleteDir()
+            }
+        }
+
         project.getExtensions().create("router", RouterExtension)
         project.afterEvaluate {
             RouterExtension extension = project["router"]
             println("用户配置的wiki目录为：${extension.wikiDir}")
+
+            //compileDebugJavaWithJavac
+            project.tasks.findAll { task ->
+                task.name.startsWith('compile') && task.name.endsWith('JavaWithJavac')
+            }.each { task ->
+                task.doLast {
+                    File routerMappingDir = new File(project.rootProject.projectDir, "router_mapping")
+                    if (!routerMappingDir.exists()) {
+                        return
+                    }
+                    File[] allChildFiles = routerMappingDir.listFiles()
+                    if (allChildFiles.length < 1) {
+                        return
+                    }
+                    StringBuilder builder = new StringBuilder("# 页面文档\n\n")
+                    allChildFiles.each { child ->
+                        if (child.name.endsWith(".json")) {
+                            JsonSlurper jsonSlurper = new JsonSlurper()
+                            def content = jsonSlurper.parse(child)
+                            content.each { innerContent ->
+                                def description = innerContent['description']
+                                def realPath = innerContent['realPath']
+                                def url = innerContent['url']
+                                builder.append("## $description\n")
+                                builder.append("- url: $url\n")
+                                builder.append("- realPath: $realPath\n\n")
+                            }
+                        }
+                    }
+                    File wikiFile = new File(project.rootProject.projectDir, "页面文档.md")
+                    if (wikiFile.exists()) {
+                        wikiFile.delete()
+                    }
+                    wikiFile.write(builder.toString())
+                }
+            }
+
         }
     }
 }
