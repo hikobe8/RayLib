137d55be5026d142ebb18bee011d3de196ce1cf3
hikobe8 <hikobe8@163.com>
2021-10-26 18:52:33 +0800

    doc

---
+++ performance/src/main/java/com/ray/performance/Main2Activity.kt
@@ -0,0 +1,12 @@
+package com.ray.performance
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Main2Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Thread.sleep(200)
+    }
+}
---
+++ performance/src/main/java/com/ray/performance/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.ray.performance
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        startActivity(Intent(this, Main2Activity::class.java))
+    }
+}
