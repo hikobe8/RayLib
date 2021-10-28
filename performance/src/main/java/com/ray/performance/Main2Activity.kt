c124604a9112b71789c9805a4cd40d68b0ac0fda
hikobe8 <hikobe8@163.com>
2021-10-28 19:17:04 +0800

    some notes

--- performance/src/main/java/com/ray/performance/Main2Activity.kt
+++ performance/src/main/java/com/ray/performance/Main2Activity.kt
@@ -1,12 +1,22 @@
 package com.ray.performance
 
+import android.content.Context
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.util.AttributeSet
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import androidx.annotation.RequiresApi
+import androidx.core.view.LayoutInflaterCompat
 
 class Main2Activity : AppCompatActivity() {
+    @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Thread.sleep(200)
     }
 }
